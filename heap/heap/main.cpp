7e9ec65d5daf616190651df62f066d69dc4054f3
songnari <na061300@naver.com>
2017-11-26 16:17:05 +0900

    기본 조건 작성

---
+++ heap/heap/main.cpp
@@ -0,0 +1,9 @@
+//Input : heap operations of size n from the file (n : 1~150)
+//        input.txt 파일로 입력받음.
+//        1~9 & a~z & A~Z 입력가능.
+//		  I 5 -> heap에 5 입력
+//        D -> heap의 root노드 삭제
+//Output : the final shape of the constructed heap in given three forms
+//         1:rotated form
+//		   2:non-rotated form
+//		   3:H-tree form
